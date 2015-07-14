af3c2811473c223c8b1371ba026ed28cd882ed74
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2015-07-13 23:50:26 -0700

    update to use templatebuilder 1.1.4.0-beta

--- Source/MVC6/Boilerplate.Web.Mvc6.Sample/wwwroot/_references.js
+++ Source/MVC6/Boilerplate.Web.Mvc6.Sample/wwwroot/_references.js
@@ -9,5 +9,5 @@
 /// <reference path="js/jquery-validate.js" />
 /// <reference path="js/jquery-validate-unobtrusive.js" />
 /// <reference path="js/hammer.js" />
-/// <reference path="js/bootstrap.js" />
 /// <reference path="js/bootstrap-touch-carousel.js" />
+/// <reference path="js/bootstrap.js" />
